31978dec20dd913b3fee5b5e89ca6b873c854e5b
suhyeon <kzoen0040@naver.com>
2021-11-06 01:06:20 +0900

    hw9
    hw9 강수현

---
+++ 1.c
@@ -0,0 +1,6 @@
+#include "myProject.h"
+int printMyName1(void)
+{
+    printf("1. my name is [강수현]\n");
+    return 1;
+}
---
+++ myProject.h
@@ -0,0 +1,6 @@
+#ifndef _myProject_H_
+#define _myProject_H_
+
+int printMyName1(void);
+
+#endif
