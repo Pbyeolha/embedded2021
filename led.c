48edbe72c69473688acf65d90b8b6cee420618c4
byeolha Park <park_byeolha@naver.com>
2021-11-26 16:45:04 +0900

    변수명 헷갈려서 다시 Status로 변경

--- led.c
+++ led.c
@@ -7,40 +7,37 @@
 #define LED_DRIVER_NAME "/dev/periled"
 
 static int fd = 0;
-static int ledStatus = 0;
+static unsigned int ledValue = 0;
 
 int ledLibInit(void)
 {
    fd = open(LED_DRIVER_NAME, O_RDWR);
-   ledStatus = 0;
+   ledValue = 0;
 
    if(fd < 0)
    {
       printf("led driver open error.\n");
       return 0;
    }
-   ledOffAll();
+
    return 1;
 }
 int ledOnOff(int ledNum, int onOff)
 {
    int i=1;
    i = i <<ledNum;
-   ledStatus = ledStatus & (~i);
+   ledValue = ledValue & (~i);
    if (onOff != 0)
-      ledStatus |= i;
-   write(fd, &ledStatus, 4);
+      ledValue |= i;
+   write(fd, &ledValue, 4);
    
 }
 
-// int ledValue(void)
-// {
-//     //ledStatus = 0x00;
-//    return ledStatus;
-// }
+int ledStatus(void){}
 
 int ledLibExit(void)
 {
-   ledOffAll();
+   ledValue=0;
+   ledOnOff(0,0);
    close(fd);
 }
