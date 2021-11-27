cde9103938b04d22b0c6fae1fda8783658b54b14
suhyeon <kzoen0040@naver.com>
2021-11-28 01:01:23 +0900

    led.c 수정
    드럼 변수 추가하고 그에따른 ledstatus 수정.

--- led.c
+++ led.c
@@ -8,6 +8,8 @@
 
 static int fd = 0;
 static unsigned int ledValue = 0;
+static int bass, snare, hi_hat, tomtom, crash, ride, floor_tom=0; //드럼구성, global변수, 다른 app, lib에서도 사용 가능
+int i=1;
 
 int ledLibInit(void)
 {
@@ -19,12 +21,11 @@ int ledLibInit(void)
       printf("led driver open error.\n");
       return 0;
    }
-
+   
    return 1;
 }
 int ledOnOff(int ledNum, int onOff)
 {
-   int i=1;
    i = i <<ledNum;
    ledValue = ledValue & (~i);
    if (onOff != 0)
@@ -34,7 +35,13 @@ int ledOnOff(int ledNum, int onOff)
 }
 
 int ledStatus(void){
-   for(int i=0; i<8; i++) printf("%d led is on!\n", i);
+   if(i==1) printf("Bass Drum\n");
+   else if(i==2) printf("Snare Drum\n");
+   else if(i==3)   printf("Hi-Hat\n");
+   else if(i==4)   printf("Tom Tom\n");
+   else if(i==5)   printf("Floor Tom\n");
+   else if(i==6) printf("Crash Cymbal\n");
+   else if(i==7)  printf("Ride Cymbal\n");
 }
 
 int ledLibExit(void)
@@ -43,3 +50,4 @@ int ledLibExit(void)
    ledOnOff(0,0);
    close(fd);
 }
+
