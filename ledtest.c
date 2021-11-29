fdfd3e0433a078e947af21170c273ca03db441d9
suhyeon <kzoen0040@naver.com>
2021-11-29 17:48:26 +0900

    [ledtest.c] 주석 처리 수정

--- ledtest.c
+++ ledtest.c
@@ -19,19 +19,20 @@ int main(int argc, char **argv){
     unsigned int data = strtol(argv[1], NULL, 16);
 
     ledLibInit();
-    // for(int i=0;i<8;i++)
-    // {
-    //     if(i==0) ledOnOff(0,1);
-    //     else if(i==1) ledOnOff(1,1);
-    //     else if(i==2)   ledOnOff(2,1);
-    //     else if(i==3)   ledOnOff(3,1);
-    //     else if(i==4)   ledOnOff(4,1);
-    //     else if(i==5) ledOnOff(5,1);
-    //     else if(i==6)  ledOnOff(6,1);
-    //     else if(i==7)  ledOnOff(7,1);
-    // }
-    ledOnOff(data);
+    for(int i=0;i<8;i++)
+    {
+        if(i==0) ledOnOff(0,1);
+        else if(i==1) ledOnOff(1,1);
+        else if(i==2)   ledOnOff(2,1);
+        else if(i==3)   ledOnOff(3,1);
+        else if(i==4)   ledOnOff(4,1);
+        else if(i==5) ledOnOff(5,1);
+        else if(i==6)  ledOnOff(6,1);
+        else if(i==7)  ledOnOff(7,1);
+    }
+    //ledOnOff(data);
     ledStatus();
+    sleep(10);
     ledLibExit();
     return 0;
 }
