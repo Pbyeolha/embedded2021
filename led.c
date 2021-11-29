99a1d8b0dec8cd69362bc9c22e77f0bc2120f102
suhyeon <kzoen0040@naver.com>
2021-11-29 18:29:14 +0900

    [led.c] exit <-> status 수정

--- led.c
+++ led.c
@@ -14,16 +14,16 @@ int ledOnOff(int ledNum,int onOff)
 {
    int i;
    i = i <<ledNum;
-   ledValue = ledValue & (~i);
    if (onOff != 0)
     ledValue |= i;
+   else ledValue = ledValue & (~i);
    //ledValue=ledNum;
    write(fd, &ledValue, 4);
 }
 
 int ledLibInit(void)
 {
-   fd = open(LED_DRIVER_NAME, O_WRONLY);
+   fd = open(LED_DRIVER_NAME, O_RDWR);
    ledValue = 0;
 
    if(fd < 0)
@@ -35,20 +35,16 @@ int ledLibInit(void)
    return 1;
 }
 
-int ledStatus(void){
-   if(ledValue==0x01) printf("Bass Drum\n");
-   else if(ledValue==0x02) printf("Snare Drum\n");
-   else if(ledValue==0x03)   printf("Hi-Hat\n");
-   else if(ledValue==0x04)   printf("Tom Tom\n");
-   else if(ledValue==0x10)   printf("Floor Tom\n");
-   else if(ledValue==0x20) printf("Crash Cymbal\n");
-   else if(ledValue==0x30)  printf("Ride Cymbal\n");
-}
-
 int ledLibExit(void)
 {
-   // ledValue=0;
-   // ledOnOff(0,0);
+   ledValue=0;
+   ledOnOff(0,0);
    close(fd);
 }
 
+int ledStatus(void){
+   for(int i=0;i<8;i++)
+	{
+		printf("%d led on :%d\n",i,(ledValue>>i)&1);
+	}
+}
