233292b94c231d40ab9454b5d9df32ccded00194
Kang Suhyeon <kzoen0040@naver.com>
2021-11-30 18:10:19 +0900

    button.h 수정- 순서변경

--- button.h
+++ button.h
@@ -1,15 +1,13 @@
 #ifndef _BUTTON_H_
 #define _BUTTON_H_
-
-int buttonLibInit(void);
-int buttonLibExit(void);
-
 #define MESSAGE_ID 1122
-
 typedef struct{
     long int messageNum;
     int keyInput;
     int pressed;
 }BUTTON_MSG_T;
+
+int buttonLibInit(void);
+int buttonLibExit(void);
 
 #endif _BUTTON_H_
