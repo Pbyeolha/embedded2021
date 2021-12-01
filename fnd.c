ebe3e0e10d060157ecae035d5de381b7e781f598
byeolha Park <park_byeolha@naver.com>
2021-12-01 18:29:03 +0900

    [fnd.h] 강의자료에 있는 추가 기능 함수 추가

--- fnd.c
+++ fnd.c
@@ -60,125 +60,40 @@ int fndDisp(int num , int dotflag)
 	return 1;
 }
 
-int fndOff()
-{
-	int fd,i;
-	stFndWriteForm stWriteData;
-	
-	for (i = 0; i < MAX_FND_NUM ; i++ )
-	{
-		stWriteData.DataDot[i] =  0;  
-		stWriteData.DataNumeric[i] = 0;
-		stWriteData.DataValid[i] = 0;
-	}
-	fd = open(FND_DRIVER_NAME,O_RDWR);
-	if ( fd < 0 )
-	{
-		perror("driver open error.\n");
-		return 0;
-	}	
-	write(fd,&stWriteData,sizeof(stFndWriteForm));
-	close(fd);
-	return 1;
-}
-
-
-
 #define MODE_STATIC_DIS		0
 #define MODE_TIME_DIS		1
 #define MODE_COUNT_DIS		2
-int main(int argc , char **argv)
-{
-	int mode ;
-	int number,counter;
-	
-	if (argc <  2)
-	{
-		doHelp();
-		return 1;
-	}
-	
-	if ( argv[1][0] == 's'  )
-	{
-		mode = MODE_STATIC_DIS;
-		if ( argc < 3 )
-		{
-			perror(" Args number is less than 3\n");
-			doHelp();
-			return 1;
-		}
-		number = atoi(argv[2]);
-	}
-	else if ( argv[1][0] == 't'  )
-	{
-		mode = MODE_TIME_DIS;
-		if ( argc < 2 )
-		{
-			perror(" Args number is less than 2\n");
-			doHelp();
-			return 1;
-		}
-	}
-	else if ( argv[1][0] == 'c'  )
-	{
-		mode = MODE_COUNT_DIS;
-		if ( argc < 3 )
-		{
-			perror(" Args number is less than 3\n");
-			doHelp();
-			return 1;
-		}
-		number = atoi(argv[2]);
-	}
-	else if (argv[1][0] == 'o' )
-	{
-		fndOff();
-		return 0; 
-	}
-	else
-	{
-		doHelp();
-		perror("option error \n");
-		return 1;
-	}
-	printf("number :%d\n",number);
-	// open  driver 
-	if (mode == MODE_STATIC_DIS )
-	{
-		fndDisp(number , 0);
-	}
-	else if(mode == MODE_TIME_DIS )
-	{
-		struct tm *ptmcur;
-		time_t tTime;
-		if ( -1 == time(&tTime) )
-			return -1; 
 
-		ptmcur = localtime(&tTime);
+int fndOther(int mode, int number){
+    int counter;
 
-		number = ptmcur->tm_hour * 10000;
-		number += ptmcur->tm_min *100;
-		number += ptmcur->tm_sec;
+    if(mode == MODE_STATIC_DIS){
+        fndDisp(number, 0);
+    }
 
-		fndDisp(number , 0b1010);
-		
-	}
-	else if (mode == MODE_COUNT_DIS)
-	{
-		counter = 0;
+    else if(mode == MODE_TIME_DIS){
+        struct tm *ptmcur;
+        time_t tTime;
+        if(-1 == time(&tTime)) return -1;
 
-		while(1)
-		{
-			if (!fndDisp(counter , 0))
-				break;
+        ptmcur = localtime(&tTime);
 
-			counter++;
-			sleep(1);
-			if (counter > number )
-				break;
-		}
+        number = ptmcur -> tm_hour*10000;
+        number += ptmcur -> tm_min*100;
+        number += ptmcur -> tm_sec;
 
-	}
+        fndDisp(number, 0b1010);
+    }
+    
+    else if(mode==MODE_COUNT_DIS){
+        counter = 0;
+        while(1){
+            if(!fndDisp(counter, 0)) break;
+            counter ++;
+            sleep(1);
+            if(counter>number) break;
+        }
+    }
 	
 	return 0;
 }
