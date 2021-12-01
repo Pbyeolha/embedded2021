5f129aadc5b3ad88eeb8601507f02749f7aebdb8
byeolha Park <park_byeolha@naver.com>
2021-12-01 19:01:53 +0900

    [fnd.c] doHlep 함수 삭제

--- fnd.c
+++ fnd.c
@@ -17,20 +17,6 @@
 #define MODE_COUNT_DIS		2
 #define FND_DRIVER_NAME		"/dev/perifnd"
 
-void doHelp(void)
-{
-	printf("Usage:\n");
-	printf("fndtest [option] [displaynum] [maxcounter]  >\n");
-	printf("option   s  : static display  , displaynum range 0 ~ 999999\n");
-	printf("option   t  ; time display  \n"); 
-	printf("option   c  : count from 0 to maxcounter .\n");
-	printf("option   o  : fnd off\n");
-	printf("ex) fndtest s '123456'  ; display  '123456' \n");
-	printf("ex) fndtest t 		;display current time \n");
-	printf("ex) fndtest c 10 ; display counting number  from  0  to  10  with 1 Second interval.\n");
-	printf("ex) fndtest 0		;display off \n");
-}
-// return 1 => success  , 0 => error
 int fndDisp(int num , int dotflag)
 {
 	int fd;
