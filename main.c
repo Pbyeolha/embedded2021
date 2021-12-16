6ae76cafbdacc6bd306acadb031527f1ebd5a59a
byeolha Park <park_byeolha@naver.com>
2021-12-16 16:17:12 +0900

    스테이지 원 수정

--- main.c
+++ main.c
@@ -38,6 +38,19 @@ static void *thread_song()
     sleep(1);
     while(1)
     {
+        for (int i = 0; i < 8; i++)
+        {
+            ledOnOff(i, 1);
+            ledStatus();
+            sleep(1);
+        }
+        for (int i = 0; i < 8; i++)
+        {
+            ledOnOff(i, 0);
+            ledStatus();
+            sleep(1);
+        }
+    }
     buzzerPlaySong(2); //미
     sleep(1);
     buzzerStopSong();
@@ -145,8 +158,6 @@ int main(int argc, char *argv[]){
     //=============song==============//
       pthread_create(&thread[0], NULL, &thread_song, NULL);
       pthread_join(thread_song,NULL);
-
-
     //=============init=============//
     ledLibInit();
     textlcdLibInit();
@@ -161,7 +172,6 @@ int main(int argc, char *argv[]){
     int returnValue = 0; 
     int returnValue1 = 1;
     BUTTON_MSG_T messageRx;
-    TOUCH_MSG_T messageRxt;
 
     returnValue=msgrcv(msgID, &messageRx, sizeof(messageRx.messageNum), 0 , 0);
    
@@ -170,42 +180,90 @@ int main(int argc, char *argv[]){
 
       switch(messageRx.keyInput)
         {
-            case KEY_VOLUMEUP:   printf("Volume up key):"); stage4=1;  break;
-            case KEY_HOME:      printf("Home key):"); start=1;     break;
-            case KEY_SEARCH:   printf("Search key):"); stage2=1;     break;
-            case KEY_BACK:      printf("Back key):"); stage1=1;     break;
-            case KEY_MENU:      printf("Menu key):"); stage3=1;     break;
-            case KEY_VOLUMEDOWN:printf("Volume down key):");break;
+            case KEY_VOLUMEUP:   printf("STAGE 4"); stage4=1;  break;
+            case KEY_HOME:      printf("GAME START"); start=1;     break;
+            case KEY_SEARCH:   printf("STAGE 2"); stage2=1;     break;
+            case KEY_BACK:      printf("STAGE 1"); stage1=1;     break;
+            case KEY_MENU:      printf("STAGE 3"); stage3=1;     break;
+            case KEY_VOLUMEDOWN:printf("GAME FINISH");break;
         }
         printf("\n");
         if(start ==1){ //start
-            ledOnOff(8, 1);
             lcdtextwrite("1", "hello!\n");
             lcdtextwrite("2", "Have a nice playing\n");
         }  
 
         if(stage1 ==1){ //stage1
-            ledOnOff(2, 1);
-
-            fndDisp(2, 0); //그림 2개 맞춰야 함
-            show_bmp("./flower.bmp");
-            returnValue1= msgrcv(msgIDt, &messageRxt, sizeof(messageRxt.messageNum), 0, 0);
-            if(messageRxt.messageNum == 1) //x좌표 
-                printf("x: %d \n", messageRxt.touchX);
-            else if(messageRxt.messageNum == 2) //y좌표
-                printf("y: %d \n", messageRxt.touchY);
-
+            lcdtextwrite("1", "STAGE 1\n");
+            show_bmp("stage1_menual.bmp");
+            sleep(5);
+            show_bmp("stage1_question.bmp");
+            sleep(5);
+            lcdtextwrite("2", "press button where is question's picture \n");
+            show_bmp("stage1_1.bmp");
+            fndDisp(1, 0);
+            sleep(3);
+            show_bmp("stage1_2.bmp");
+            fndDisp(2, 0);
+            sleep(3);
+            show_bmp("stage1_3.bmp");
+            fndDisp(3,0);
+            sleep(3);
+            
+            if(stage2 == 1){ //두번째 button 눌렸을 때
+                fndDisp(2, 0);
+                show_bmp("stage_correct.bmp");
+                lcdtextwrite("1", "Correct!\n");
+                lcdtextwrite("2", "Go to Next stage!\n");
+            }
+            else if(stage1 == 1){ //첫번째 button 눌렸을 때
+                fndDisp(1, 0);
+                show_bmp("wrong.bmp");
+                lcdtextwrite("1", "Sorry\n");
+                lcdtextwrite("2", "You're wrong\n")
+            }
+            else if(stage3 == 1){ //세번째 button 눌렸을 때
+                fndDisp(3, 0);
+                show_bmp("wrong.bmp");
+                lcdtextwrite("1", "sorry\n");
+                lcdtextwrite("2", "You're wrong\n");
+            }
         }
         else if(stage2 ==1){ //stage2
-            ledOnOff(4, 1);
-            fndDisp(3, 0); //그림 3개 맞춰야 함
-            show_bmp("./flower.bmp");
-            returnValue1 = msgrcv(msgIDt, &messageRxt, sizeof(messageRxt.messageNum), 0, 0);
-            if(messageRxt.messageNum == 1) //x좌표 
-                printf("x: %d \n", messageRxt.touchX);
-            else if(messageRxt.messageNum == 2) //y좌표
-                printf("y: %d \n", messageRxt.touchY);
-
+            lcdtextwrite("1", "STAGE 2\n");
+            show_bmp("stage2_menual.bmp");
+            sleep(5);
+            show_bmp("stage2_question.bmp");
+            sleep(5);
+            lcdtextwrite("2", "press button Who am I \n");
+            show_bmp("stage2_1.bmp");
+            fndDisp(1, 0);
+            sleep(3);
+            show_bmp("stage2_2.bmp");
+            fndDisp(2, 0);
+            sleep(3);
+            show_bmp("stage2_3.bmp");
+            fndDisp(3,0);
+            sleep(3);
+            
+            if(stage1 == 1){ //첫번째 button 눌렸을 때
+                fndDisp(1, 0);
+                show_bmp("stage2_correct.bmp");
+                lcdtextwrite("1", "Correct!\n");
+                lcdtextwrite("2", "Go to Next stage!\n");
+            }
+            else if(stage2 == 1){ //두번째 button 눌렸을 때
+                fndDisp(2, 0);
+                show_bmp("stage2_uncorrect.bmp");
+                lcdtextwrite("1", "Sorry\n");
+                lcdtextwrite("2", "You're wrong\n")
+            }
+            else if(stage3 == 1){ //세번째 button 눌렸을 때
+                fndDisp(3, 0);
+                show_bmp("stage2_uncorrect.bmp");
+                lcdtextwrite("1", "sorry\n");
+                lcdtextwrite("2", "You're wrong\n");
+            }
         }
         else if(stage3 ==1){ //stage3
  
